ae90ba96958b267c9a1b1f73accfed094f07376e
mikemospan <28594918+mikemospan@users.noreply.github.com>
2025-07-01 15:34:36 +1000

    fix: fixed from previous version

--- example/core/pd1.c
+++ example/core/pd1.c
@@ -57,8 +57,7 @@ void notified(microkit_channel ch) {
         break;
     case 'y':
         microkit_dbg_puts("[PD 1]: Turning on core #1\n");
-        int (*cpu_bootstrap)(int) = (int(*)(int)) (0x70000e5c);
-        cpu_bootstrap(1);
+        core_on(1, 0x70000e5c);
         break;
     case 'i':
         microkit_dbg_puts("[PD 1]: Viewing status of core #0\n");
