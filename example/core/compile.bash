f6cf0aebc20af8c1e17f88028dfe73af23050bf7
mikemospan <28594918+mikemospan@users.noreply.github.com>
2025-09-21 11:29:32 +1000

    feat: improved compile.bash

--- example/core/compile.bash
+++ example/core/compile.bash
@@ -1,13 +1,19 @@
-# !/bin/bash
+#!/bin/bash
 
 # Set the environment variables for the build
+BUILD_DIR="./build"
 MICROKIT_BOARD="qemu_virt_aarch64"
 MICROKIT_SDK="../../release/microkit-sdk-2.0.1-dev"
 MICROKIT_CONFIG="debug"
 NUM_CPUS=4
 
+# Ensure the script is run from the correct directory
+if ! pwd | grep -q "example/core"; then
+    echo "Please run this script from the example/core directory."
+    exit 1
+fi
+
 # Check if the build directory exists; if not, create it
-BUILD_DIR="./build"
 if [ ! -d "$BUILD_DIR" ]; then
     mkdir -p "$BUILD_DIR"
 fi
